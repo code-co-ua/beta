181caadf283d20579562f19eeb6912ff21edcb35
Ostap Bregin <osbre@protonmail.com>
2021-12-15 02:37:04 +0200

    wip

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -21,8 +21,7 @@ createInertiaApp({
     setup({el, app, props, plugin}) {
         return createApp({render: () => h(app, props)})
             .use(plugin)
-            .component('Link', Link)
-            .component('Head', Head)
+            .mixin({ methods: { route } })
             .mount(el)
     },
 })
